c691f237573b0edfcdf40853f2f4e139b5d35be6
Ackeem Smith <ackeem.smith@mymona.uwi.edu>
2019-03-03 20:42:47 +0000

    Secure site

--- app/views.py
+++ app/views.py
@@ -10,7 +10,7 @@ from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, current_user, login_required
 from app.forms import LoginForm
 from app.models import UserProfile
-
+from werkzeug.security import check_password_hash
 
 ###
 # Routing for your application.
@@ -31,24 +31,36 @@ def about():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
-    if request.method == "POST":
+    if request.method == "POST" and form.validate_on_submit():
         # change this to actually validate the entire form submission
         # and not just one field
-        if form.username.data:
-            # Get the username and password values from the form.
 
-            # using your model, query database for a user based on the username
-            # and password submitted. Remember you need to compare the password hash.
-            # You will need to import the appropriate function to do so.
-            # Then store the result of that query to a `user` variable so it can be
-            # passed to the login_user() method below.
+        # Get the username and password values from the form.
+        username = form.username.data
+        password = form.password.data
 
+        # using your model, query database for a user based on the username
+        # and password submitted. Remember you need to compare the password hash.
+        # You will need to import the appropriate function to do so.
+        # Then store the result of that query to a `user` variable so it can be
+        # passed to the login_user() method below.
+        user = UserProfile.query.filter_by(username=username).first()
+        if user is not None and check_password_hash(user.password, password):
             # get user id, load into session
             login_user(user)
-
+            next_page = request.args.get('next')
             # remember to flash a message to the user
-            return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
+            flash("Login Successful", "success")
+            print next_page
+            return redirect(url_for("secure"))  # they should be redirected to a secure-page route instead
+            
+        flash("login failed", 'danger')
     return render_template("login.html", form=form)
+    
+@app.route('/secure-page')
+@login_required
+def secure():
+    return render_template('secure.html')
 
 
 # user_loader callback. This callback is used to reload the user object from
